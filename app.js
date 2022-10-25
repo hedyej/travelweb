a0a6b6ec1bb1132bd2f40ac8eb32711c1ffba0d7
Hedy <duej123456@gmail.com>
2022-10-25 16:12:48 +0800

    第二版 改用innerHTML處理頁面渲染

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 
-//選擇element
-const selection = document.querySelector(".search .area")
-
 //接API
 async function getOption(){
     const res = await axios.get("https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json")
     const spot = res.data.result.records
 
-    //篩選selection option
+
+ //處理Selection
+    //篩選行政區
     const pastZone = spot.map(item => item.Zone)
     const newZone = pastZone.filter((item,index)=>{
         return pastZone.indexOf(item) === index
     })
     optionList = newZone 
 
-    //放入selection option
+    //選擇selection
+    const selection = document.querySelector(".search .area")
+
+    //放入行政區選項
     optionList.forEach(item => {
     const optionItem = document.createElement("option")
     optionItem.innerText = item
     selection.appendChild(optionItem)  
     })
    
+//選擇行政區
     selection.addEventListener("change",e => {
-        //依據選擇顯示資訊
+        //查看選擇
         const optionArea = e.target.value
 
-        //讓原本卡片消失？
+        let fitSpot = []
+        spot.forEach((item)=>{
+            if(item.Zone === optionArea){
+                fitSpot.push(item) 
+            }
+        })
+
+        //讓上個行政區卡片消失
         const resultGrid = document.querySelector(".resultGrid")
         resultGrid.innerHTML =" "
-        
-        let areaList = []
-        for(i=0; i<spot.length; i++){
-            if(spot[i].Zone === optionArea){
-                areaList.push(spot[i])
-            }
-        }
 
-        //取得資料
-        areaList.forEach(item =>{
+
+//卡片群組
+        let spotGroup = ""
+
+        fitSpot.forEach(item =>{
             const ticket = item.Ticketinfo
             const zone = item.Zone
             const add = item.Add
@@ -45,90 +51,30 @@ async function getOption(){
             const name = item.Name
             const img = item.Picture1
             const tel = item.Tel
-
-            //建立element
-            const resultSection = document.querySelector(".resultSection")
-
-            const cardDeck = document.createElement("div")
-            cardDeck.classList.add("card-deck","h-100")
-
-            const cardItem = document.createElement("div")
-            cardItem.classList.add("card","p-0")
-
-            const spotImg = document.createElement("img")
-            spotImg.classList.add("imgCover","imgHeight","card-img-top","position-relative")
-            spotImg.setAttribute("src",img)
-
-            const titleSection = document.createElement("div")
-            titleSection.classList.add("title","d-flex","justify-content-between","align-items-end","px-3","imgHeight","position-absolute","z-index-1")
-
-            const title = document.createElement("h5")
-            title.classList.add("text-light")
-            title.textContent = name
             
-            const des = document.createElement("p")
-            des.classList.add("mb-2","text-light")
-            selection.setAttribute("style","line-height:28px")
-            des.textContent = zone     
-            
-            const cardBody  = document.createElement("div")
-            cardBody.classList.add("card-body")
-
-            const timeInfo = document.createElement("p")
-            timeInfo.textContent = time
-            
-
-            const timeImg = document.createElement("img")
-            timeImg.classList.add("icSize")
-            timeImg.setAttribute("src","assets/icons_clock.png")
-
-            const addInfo = document.createElement("p")
-            addInfo.textContent = add
-
-            const addImg = document.createElement("img")
-            addImg.classList.add("icSize")
-            addImg.setAttribute("src","assets/icons_pin.png")
-
-            const lastLine = document.createElement("div")
-            lastLine.classList.add("d-flex","justify-content-between")
-
-            const phoneInfo = document.createElement("p")
-            phoneInfo.textContent = tel
-
-            const phoneImg = document.createElement("img")
-            phoneImg.classList.add("icSize")
-            phoneImg.setAttribute("src","assets/icons_phone.png")
-
-            const tag = document.createElement("p")
-            tag.setAttribute("style","margin-bottom")           
-            tag.textContent = ticket
-
-            const tagImg = document.createElement("img")
-            tagImg.classList.add("icSize")
-            tagImg.setAttribute("src","assets/icons_tag.png")
-            
-
-
-            //appendChild
-            console.log(resultSection)
-            resultSection.appendChild(resultGrid)
-            resultGrid.appendChild(cardDeck)
-            cardDeck.appendChild(cardItem)
-            cardItem.appendChild(spotImg)
-            cardItem.appendChild(titleSection)
-            titleSection.appendChild(title)
-            titleSection.appendChild(des) 
-            cardItem.appendChild(cardBody)
-            cardBody.appendChild(timeInfo)
-            timeInfo.appendChild(timeImg)
-            cardBody.appendChild(addInfo)
-            addInfo.appendChild(addImg)
-            cardBody.appendChild(lastLine)
-            lastLine.appendChild(phoneInfo)
-            phoneInfo.appendChild(phoneImg)
-            lastLine.appendChild(tag)
-            tag.appendChild(tagImg)
+            spotGroup += `
+                <div class="card-deck">
+                <div class="card p-0">
+                    <img class="imgCover imgHeight card-img-top position-relative " src="${img}">
+                    <div class="title text-light d-flex justify-content-between align-items-end px-3 imgHeight position-absolute z-index-1">
+                        <h4> ${name} </h4>
+                        <p style="line-height:28px;" class="mb-2"> ${zone} </p>
+                    </div>
+                    <div class="card-body">
+                        <p><img class="icSize" src="assets/icons_clock.png"> ${time} </p>
+                        <p><img class="icSize" src="assets/icons_pin.png"> ${add} </p>
+                        <div class="d-flex justify-content-between">
+                            <p><img class="icSize" src="assets/icons_phone.png"> ${tel} </p>
+                            <p style="margin-bottom:0px"><img class="icSize" src="assets/icons_tag.png"> ${ticket}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>        
+            `
         })
+
+        const grid = document.querySelector(".resultGrid")
+        grid.innerHTML = spotGroup
         })
 }
 
